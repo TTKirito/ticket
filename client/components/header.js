a3863a7d01ec5af5ff5e766862df085945f698ea
TTKirito <thuanton98@gmail.com>
2020-12-04 15:03:56 +0700

    test5

--- client/components/header.js
+++ client/components/header.js
@@ -5,8 +5,8 @@ const header = ({currentUser}) =>{
     const links = [
         !currentUser && { label: 'Sign Up', href:"/auth/signup"},
         !currentUser && { label: 'Sign In', href:"/auth/signin"},
-        currentUser && { label: 'New Ticket', href:"/tickets/new"},
-        currentUser && { label: 'My Orders', href:"/orders"},
+        currentUser && { label: 'New Tickets', href: '/tickets/new' },
+        currentUser && { label: 'My Orders', href: '/orders' },
         currentUser && { label: 'Sign Out', href:"/auth/signout"}
 
     
