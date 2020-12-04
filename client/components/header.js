7cee6293150c023cd9431a547e5a7e6cac129948
TTKirito <thuanton98@gmail.com>
2020-12-04 16:10:02 +0700

    thuan222

--- client/components/header.js
+++ client/components/header.js
@@ -1,11 +1,10 @@
 import Link from 'next/link'
-
+import google from '../pages/auth/google'
 const header = ({currentUser}) =>{
 
     const links = [
         !currentUser && { label: 'Sign Up', href:"/auth/signup"},
         !currentUser && { label: 'Sign In', href:"/auth/signin"},
-        !currentUser && { label: 'Google Login', href:"/auth/google"},
         currentUser && { label: 'New Tickets', href: '/tickets/new' },
         currentUser && { label: 'My Orders', href: '/orders' },
         currentUser && { label: 'Sign Out', href:"/auth/signout"}
@@ -35,6 +34,7 @@ const header = ({currentUser}) =>{
             <div className="d-flex justify-context-end">
                 <ul className="nav align-items-center">
                     {links}
+                    {!currentUser && google()}
                 </ul>
             </div>
         </nav>
